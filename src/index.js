2fd5a1a46c2d7aa134d382c8e81323a572b94d52
kazmonroy <katherinemonroy@gmail.com>
2022-09-20 15:35:48 +0200

    Add modules

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 import "./style.css";
+import loadPage from "./load-page.js";
+import aboutUsPage from "./about-us.js";
+
+const content = document.querySelector("#content");
+
+function init() {
+  loadPage();
+}
+
+init();
+
+const homeBtn = document.querySelector("#home");
+const aboutUsBtn = document.querySelector("#about-us");
+const menuBtn = document.querySelector("#menu");
+
+function navigate() {
+  aboutUsBtn.addEventListener("click", aboutUsPage);
+}
